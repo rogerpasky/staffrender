3d97b3cee2ebf0b4a6f01f4c61f330485b469497
rogerpasky <pascual.dejuan@bbva.com>
2021-01-17 00:30:44 +0100

    Rearranged library exports

--- src/index.ts
+++ src/index.ts
@@ -16,5 +16,6 @@
  */
 
 export * from './staff_svg_render';
-export { StaffInfo, NoteInfo } from './staff_info';
 export * from './staff_model';
+export * from './staff_block';
+export * from './staff_info';
